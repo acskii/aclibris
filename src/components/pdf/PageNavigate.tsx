72b1c623f92b1181c0990beac87d655ff688dcd4
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-09-14 17:42:26 +0300

    [component] Page Navigation Component for PDFViewer

---
+++ src/components/pdf/PageNavigate.tsx
@@ -0,0 +1,95 @@
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, Home } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+type PageNavigateProps = {
+    current: number;
+    total: number;
+    file: string;
+}
+
+export function PageNavigate({ current, total, file }: PageNavigateProps) {
+  const progress = (current / total) * 100;
+  const navigate = useNavigate();
+
+  const jumpToStart = () => {
+    navigate(`/view/1`, { state: { file } });
+  };
+
+  const jumpToEnd = () => {
+    navigate(`/view/${total}`, { state: { file } });
+  };
+
+  const jumpToNext = () => {
+    const jump = current + 1 > total ? total : current + 1;
+    navigate(`/view/${jump}`, { state: { file } });
+  };
+
+  const jumpToPrevious = () => {
+    const jump = current - 1 <= 0 ? 1 : current - 1;
+    navigate(`/view/${jump}`, { state: { file } });
+  };
+
+  const jumpToHome = () => {
+    navigate("/");
+  }
+
+  return (
+    <div className="fixed top-0 left-1/2 transform -translate-x-1/2 z-30 w-3/5">
+      <div className="max-w-full bg-gradient-to-b from-violet-700 via-sky-500 to-cyan-400 overflow-hidden text-white rounded-b-xl flex flex-col items-center backdrop-blur-md border border-blue-700">
+        {/* Navigation Controls */}
+        <div className="flex flex-row gap-2 items-center justify-between w-full px-4 py-2">
+          <button
+              onClick={jumpToHome}
+              className="bg-gradient-to-br from-blue-800 to-indigo-900 p-2 rounded-md hover:from-violet-400 hover:to-purple-500 text-white transition disabled:opacity-30"
+          >
+              <Home size={20} />
+          </button>
+          <div className="flex flex-row gap-2 items-center justify-center">
+            <button
+              onClick={jumpToStart}
+              disabled={current <= 1}
+              className="bg-gradient-to-br from-blue-800 to-indigo-900 p-2 rounded-md hover:from-violet-400 hover:to-purple-500 text-white transition disabled:opacity-30"
+            >
+              <ChevronsLeft size={20} />
+            </button>
+            <button
+              onClick={jumpToPrevious}
+              disabled={current <= 1}
+              className="bg-gradient-to-br from-blue-800 to-indigo-900 p-2 rounded-md hover:from-violet-400 hover:to-purple-500 text-white transition disabled:opacity-30"
+            >
+              <ChevronLeft size={20} />
+            </button>
+            <div className="text-nowrap text-sm font-bold text-white border border-3 border-purple px-3 py-1 bg-sky-400 rounded-lg">
+              {current} / {total}
+            </div>
+            <button
+              onClick={jumpToNext}
+              disabled={current >= total}
+              className="p-2 rounded-md bg-gradient-to-br from-blue-800 to-indigo-900 hover:to-purple-500 text-white transition disabled:opacity-30"
+            >
+              <ChevronRight size={20} />
+            </button>
+            <button
+              onClick={jumpToEnd}
+              disabled={current >= total}
+              className="p-2 rounded-md bg-gradient-to-br from-blue-800 to-indigo-900 hover:from-violet-400 hover:to-purple-500 text-white transition disabled:opacity-30"
+            >
+              <ChevronsRight size={20} />
+            </button>
+          </div>
+          
+        </div>
+
+        {/* Progress Bar */}
+        {total > 0 && (
+          <div className="w-full h-2 rounded-full bg-blue-800 overflow-hidden mt-2">
+            <div
+              className="h-full bg-gradient-to-r from-violet-400 via-purple-500 to-indigo-500 rounded-full transition-all duration-300"
+              style={{ width: `${progress}%` }}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
