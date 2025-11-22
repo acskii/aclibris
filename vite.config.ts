5e8b4a991aa458e4cb11d83125afe009d893a9cf
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-11-22 17:34:05 +0200

    [fix] fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // build: {
+  //   rollupOptions: {
+  //     external: ['better-sqlite3'],
+  //       },
+  // },
+
+  define: {
+    global: 'globalThis'
+  },
+
   plugins: [
     tailwindcss(),
     react(),
